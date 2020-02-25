85e7640aa954fde51f1fac90da613edc8afaee8c
Kalonji Bankole <kkbankol@us.ibm.com>
2020-02-25 09:58:28 -0800

    adding method to train model

--- image-sync.py
+++ image-sync.py
@@ -3,6 +3,8 @@ import time
 import logging
 import requests
 import json
+from datetime import datetime
+
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 
@@ -11,13 +13,13 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
+# load config file
 f = open('./configuration.json')
 config = json.load(f)
 threshold = config['threshold']
 action = config['dataset']['action']
 url = config['credentials']['endpoint']
 dataset_name = config['dataset']['name']
-
 
 # TODO, add action logic
 # "action" determines whether to append to existing dataset, or to create a new one. "append" or "create"
@@ -61,14 +63,21 @@ def create_dataset(name):
     if r.status_code == 200:
         print(f"dataset {name} created")
         res = r.json()
-        global datasets
-        datasets.append({
+        ds = {
           "_id": res["dataset_id"],
           "name": name
-        })
+        }
+        return ds
+        # global datasets
+        # datasets.append(
+
+
+# lambda
+
+# item.get('id') == 2
 
 def get_datasets():
-    global datasets
+    # global datasets
     # headers = {
     #     'X-Auth-Token': token
     # }
@@ -83,17 +92,18 @@ def get_datasets():
                 "_id": d['_id'],
                 "categories": []
             }
-            get_dataset_categories(d['name'])
-        print(ds_ids)
+            # get_dataset_categories(d['name'])
+        # print(ds_ids)
+        return ds_ids
     else:
         print(f"error getting datasets")
         print(r.status_code)
 
-def get_dataset_categories(dataset_name):
+def get_dataset_categories(dataset_id):
     headers = {
         'X-Auth-Token': token
     }
-    dataset_id = ds_ids[dataset_name]['_id']
+    # dataset_id = ds_ids[dataset_name]['_id']
     r = requests.get(f"{url}api/datasets/{dataset_id}/categories", headers=headers, verify=False)
     if r.status_code == 200:
         # global ds_categories
@@ -130,16 +140,18 @@ def create_dataset_category(dataset_name, category_name):
     if r.status_code == 200:
         print(f"added dataset category {category_name}")
         category_id = r.json()['dataset_category_id']
-        ds_ids[dataset_name]['categories'] = {
+        d = {
             category_name: category_id
         }
-        return {
-            category_name: category_id
-        }
+        ds_ids[dataset_name]['categories'] = d
+        return d
+
 
 
 
 '''
+# examples
+
 ds_ids = {
     "dataset1": {
         id: "4n231ji-12412n",
@@ -157,11 +169,13 @@ files_to_upload = {
 
 
 
+
+
 # TODO, add logic to skip files that already exist
-def upload_files(files_to_upload):
+def upload_files(files_to_upload, dataset_name):
     print(f"uploading {len(files_to_upload.keys())} files")
     # TODO, add case if images don't have category? Or are not in folder
-    dataset_name = config['dataset']['name']
+    # dataset_name = config['dataset']['name']
     for category in files_to_upload.keys():
         print(f'uploading "{category}" images')
         dataset_id = ds_ids[dataset_name]['_id']
@@ -187,9 +201,22 @@ def upload_files(files_to_upload):
     files_to_upload = {}
         # TODO zip if more than 5 files
 
+def train_model(dataset_id, dataset_name): #action=None, strategy={}):
+    # t = date.fromtimestamp(time.time()).strftime("%m_%d_%Y_%H%M%S")
+    # ds_id = ds_ids[name]['id']
+    # dataset_id
+    # name = config['dataset']['name']
+    body = {
+        action: "create", # hardcoding to classifier for now. TODO, can add object detection if object coords are included in a json file
+        dataset_id: dataset_id,
+        # strategy: strategy,
+        name: dataset_id + t
+    }
+    r = requests.post(f"{url}api/dltasks", headers=headers, json=body, verify=False)
+    if r.status_code == 200:
+        print(f"began training model for {}")
 
-
-if not 'token' in config['credentials'].keys():
+if 'token' not in config['credentials'].keys():
     get_token(config)
     config['token'] = token
     # config['time_token_received'] =
@@ -202,36 +229,60 @@ else:
     headers = {
         'X-Auth-Token': token
     }
-get_datasets()
-# get_dataset_categories()
+
+# get_datasets()
 
 
+# get dataset information (categories, files)
+ds_ids = get_datasets()
 
+dataset_name = config['dataset']['name']
+if dataset_name not in ds_ids.keys():
+    ds_ids[dataset_name] = create_dataset(dataset_name)
+
+for name in ds_ids:
+    ds_id = ds_ids[name]['id']
+    ds_ids[name]['categories'] = get_dataset_categories(ds_id)
+    ds_ids[name]['files'] = get_dataset_files(ds_id)
+
+# return ds_ids
 
 
 # Monitor folder, trigger file upload after x number of files gets added
-global file_count
-file_count = 0
+# global file_count
+# file_count = 0
 files_to_upload = {}
+global file_upload_count
+global file_train_count
+file_upload_count = 0
+file_train_count = 0
 class Event(LoggingEventHandler):
     def on_created(self, event):
         print("file added to folder")
-        # print(dir(event))
         print(event.src_path)
         split_path = event.src_path.split('/')
         filename = split_path[-1]
         category = split_path[-2]
         if category not in files_to_upload.keys():
             files_to_upload[category] = []
-        files_to_upload[category].append(event.src_path)
-        global file_count
-        file_count += 1
         image_type = filename.split('.')[-1]
+        if image_type not in ['jpg', 'png', 'mp4']:
+            print(f"unsupported media type {image_type}")
+            return
+        files_to_upload[category].append(event.src_path)
+        file_upload_count += 1
+        file_train_count += 1
         print(f'{file_count} / {threshold}')
-        if file_count >= threshold:
-            file_count = 0
-            print(f"uploading {threshold} files")
-            upload_files(files_to_upload)
+        if file_upload_count >= upload_threshold:
+            print(f"uploading {file_upload_count} files")
+            # if action == "append":
+            upload_files(files_to_upload, config['dataset']['name'])
+            file_upload_count = 0
+        if file_train_count >= training_threshold:
+            print(f"{file_train_count} files")
+            # upload_files(files_to_upload)
+            train_model( ds_ids[dataset_name]['id'] )
+            file_train_count = 0
 
         # files.append(event.src_path)
 
