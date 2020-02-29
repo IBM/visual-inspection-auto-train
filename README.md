fb69a0877d059ace22eb9a33e73c652851375dd9
Kalonji Bankol <kkbankol@us.ibm.com>
2020-02-28 21:40:19 -0800

    adding service files, readme

--- README.md
+++ README.md
@@ -1,33 +1,181 @@
+# Upload PowerAI Vision datasets
 
-# Create a root folder
+In this code pattern, we show how to simplify the model training process in PowerAI Vision.
+
+Business users using PowerAI Vision's image classification process will need to manually upload and categorize images, which can become rather tedious when working with large datasets.
+
+Here we're providing a long-running script that'll enable the user to automate the training process for image classification models. They can upload and categorize images by simply adding images to a folder.
+
+This script works by monitoring a folder containing one or more subfolders. Each subfolder contains images, and the subfolder name should match the category of the image sets.
+
+<!-- Architecture -->
+<img src="https://i.imgur.com/Fg9j0MS.png">
+
+#  Components
+
+* [PowerAI Vision](https://www.ibm.com/products/maximo). This is an image analysis platform that allows you to build and manage computer vision models, upload and annotate images, and deploy apis to analyze images and videos.
+
+Sign up for a trial account of PowerAI vision [here](https://developer.ibm.com/linuxonpower/deep-learning-powerai/try-powerai/). This link includes options to provision a PowerAI Vision instance either locally on in the cloud.
+
+# Flow
+
+1. User copies respective images to category subfolders.
+2. Script counts number of images added, determines if image count exceeds threshold.
+3. If image count exceeds "upload" threshold, script executes a POST request uploading images for each category.
+4. If image count exceeds "train" threshold, script executes a POST request to begin training a model.
+
+
+# Prerequisites
+
+* An account on IBM Marketplace that has access to PowerAI Vision. This service can be provisioned [here](https://developer.ibm.com/linuxonpower/deep-learning-powerai/vision/access-registration-form/)
+
+* Python 3
+
+
+# Steps
+
+Follow these steps to setup and run this Code Pattern.
+1. [Clone repository](#1-clone-repository)
+2. [Create image folder and subfolders](#2-create-image-folder-and-subfolders)
+3. [Fill in configuration file](#3-fill-in-configuration-file)
+4. [Start and test script](#4-start-and-test-script)
+<!-- 5. [Register script as a service](#5-register-script-as-a-service) -->
+
+
+## 1. Clone repository
+
+```
+git clone https://github.com/ibm/powerai-data-sync
+```
+
+## 2. Create image folder and subfolders
+
+```
+mkdir images
+
+# run following command for each category in your classifier
+mkdir images/<category_name>
+```
+
+
+## 3. Fill in configuration file
+
+Copy configuration file from template.
+
+```
+cp configuration.json.template configuration.json
+```
+
+Open configuration file and fill in PowerAI Vision credentials in the "credentials" object.
+
+```
+"credentials": {
+  "endpoint": "<url>",
+  "username": "<username>",
+  "password": "<password>",
+}
+```
+
+
+Next we'll configure the classification model to be trained once enough files have been uploaded. Give the model a name, and specify an action to take. Valid actions can be to "train" a new model, "retrain" an existing model, or "nothing" if you'd rather not train a model at all. Also, the "strategy" parameter can be provided to further customize the training process, such as how many iterations, type of neural network to train, and so on. The custom training parameters can be found in the api documentation [here](https://public.dhe.ibm.com/systems/power/docs/powerai/powerai-vision-api.html#dltasks_post)
+
+```
+"model": {
+  "name": "<name_of_model>",
+  "action": "train",
+  "strategy": {}
+},
+```
+
+Specify the full path to the folder(s) that will be monitored by the script
+```
+"folders": [
+  "<path to root image folder>",
+]
+```
+
+## 4. Start and test script
+
+Start the script with the following command
+
+```
+python3 image-sync.py
+```
+
+If the script is able to successfully authenticate,
+
+<img src="https://i.imgur.com/WNHQI8x.png" />
+
+
+Next, copy a few images into the various category folders. The logs will show which files were added, and how  many more images are needed to meet the threshold.
+
+<img src="https://i.imgur.com/2v2bTHb.png" />
+
+<!-- ## 5. Register script as a service
+
+Mac OSX
+```
+```
+
+*Linux*
+Open the systemd [service file](service-files/paiv-sync.service#L3)
+Enter the path to your script in the "Service" section like so
+`Environment=SCRIPT_PATH=<script path>`
+
+Copy and enable systemd file
+```
+cp service-files/paiv-sync.service /lib/systemd/system/
+systemctl enable paiv-sync
+systemctl start paiv-sync
+```
+
+Confirm service is running
+```
+systemctl status paiv-sync
+``` -->
+
+
+<!-- # Create a root folder
 
 # Create subfolders. Script assumes subfolder will match category name.
 
 #
 
+The script will monitor a folder that has one or more subfolders. Each subfolder contains images, and the subfolder name should match the category of the image sets.
+
+
 
 ```
 {
   "credentials": {
-    "endpoint": "<powerai_vision_url>:<powerai_vision_port>",
+    "endpoint": "<url>",
+    "port": "8000",
     "username": "<username>",
     "password": "<password>",
-    "time_token_received": "",
-    "token": "35bf6b87-be86-427d-b95d-b45651b3f41e"
   },
   "recursive": 1,
-  "threshold": 3,
+  "threshold": {
+    "training": 3,
+    "upload": 3
+  },
   "skip_duplicates": 1,
   "time": 10,
   "dataset": {
     "name": "classifier",
     "action": "append"
   },
+  "model": {
+    "name": "classifier",
+    "action": "retrain"
+    "_comment": "action can be retrain, train, or nothing. default to 'train'. TODO, remember that training a new model will of course result in anew ID",
+    "strategy": {}
+  },
   "folders": [
     "/Users/kkbankol@us.ibm.com/projects/powerai-data-sync/images"
   ],
   "categories": {
     "buildings": "buildings"
+    "_comment": "this section is entirely optional, used to override category if user would prefer not to use subfolder"
   }
 }
 ```
@@ -54,4 +202,10 @@ https://stackoverflow.com/a/32440
 "comment": "// number of images that can be added until upload is triggered",
 
 
-"comment": "// time (seconds) to wait after threshold is surpassed to upload",
+"comment": "// time (seconds) to wait after threshold is surpassed to upload", -->
+
+# License
+
+This code pattern is licensed under the Apache Software License, Version 2.  Separate third party code objects invoked within this code pattern are licensed by their respective providers pursuant to their own separate licenses. Contributions are subject to the [Developer Certificate of Origin, Version 1.1 (DCO)](https://developercertificate.org/) and the [Apache Software License, Version 2](https://www.apache.org/licenses/LICENSE-2.0.txt).
+
+[Apache Software License (ASL) FAQ](https://www.apache.org/foundation/license-faq.html#WhatDoesItMEAN)
--- image-sync.py
+++ image-sync.py
@@ -4,6 +4,11 @@ import logging
 import requests
 import json
 from datetime import datetime
+from datetime import date
+import time
+from threading import Timer
+from threading import Thread
+import threading
 
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
@@ -13,21 +18,24 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
+green_text = "\033[1;32;40m"
+white_text = "\033[1;37;40m"
+yellow_text = "\033[1;33;40m"
+
 # load config file
 f = open('./configuration.json')
 config = json.load(f)
-threshold = config['threshold']
+upload_threshold = config['threshold']['upload']
+training_threshold = config['threshold']['train']
 action = config['dataset']['action']
 url = config['credentials']['endpoint']
 dataset_name = config['dataset']['name']
+action = config['dataset']['action']
 
 # TODO, add action logic
-# "action" determines whether to append to existing dataset, or to create a new one. "append" or "create"
-action = config['dataset']['action']
 
 
 # Paiv calls
-
 def get_token(config):
     credentials = config['credentials']
     body = {
@@ -44,9 +52,10 @@ def get_token(config):
         global token
         token = res['token']
         print(f"setting token {token}")
-        headers = {
-            'X-Auth-Token': token
-        }
+        # headers = {
+        #     'X-Auth-Token': token
+        # }
+        return token
     else:
         print(f"failure getting token HTTP {r.status_code}")
 
@@ -57,7 +66,7 @@ def create_dataset(name):
     #     'X-Auth-Token': token
     # }
     body = {
-        name: name
+        "name": name
     }
     r = requests.post(f"{url}api/datasets", headers=headers, json=body, verify=False)
     if r.status_code == 200:
@@ -71,7 +80,6 @@ def create_dataset(name):
         # global datasets
         # datasets.append(
 
-
 # lambda
 
 # item.get('id') == 2
@@ -81,13 +89,14 @@ def get_datasets():
     # headers = {
     #     'X-Auth-Token': token
     # }
+    # print(headers)
     r = requests.get(f"{url}api/datasets", headers=headers, verify=False)
     if r.status_code == 200:
         datasets = r.json()
         global ds_ids
         ds_ids = {}
         for d in datasets:
-            print(d)
+            # print(d)
             ds_ids[d['name']] = {
                 "_id": d['_id'],
                 "categories": []
@@ -100,9 +109,9 @@ def get_datasets():
         print(r.status_code)
 
 def get_dataset_categories(dataset_id):
-    headers = {
-        'X-Auth-Token': token
-    }
+    # headers = {
+    #     'X-Auth-Token': token
+    # }
     # dataset_id = ds_ids[dataset_name]['_id']
     r = requests.get(f"{url}api/datasets/{dataset_id}/categories", headers=headers, verify=False)
     if r.status_code == 200:
@@ -112,16 +121,14 @@ def get_dataset_categories(dataset_id):
         categories = {}
         for c in ds_categories:
             categories[c['name']] = c['_id']
-
         ds_ids[dataset_name]['categories'] = categories
         print(f"dataset {dataset_id} categories received {categories}")
         return categories
     else:
         print(f"error getting dataset categories {r.status_code}")
 
-
-def get_dataset_files(dataset_name):
-    dataset_id = ds_ids[dataset_name]['_id']
+def get_dataset_files(dataset_id):
+    # dataset_id = ds_ids[dataset_name]['_id']
     r = requests.get(f"{url}api/datasets/{dataset_id}/files", headers=headers, verify=False)
     if r.status_code == 200:
         dataset_files = r.json()
@@ -143,15 +150,107 @@ def create_dataset_category(dataset_name, category_name):
         d = {
             category_name: category_id
         }
-        ds_ids[dataset_name]['categories'] = d
+        # ds_ids[dataset_name]['categories'] = d
         return d
 
+# TODO, add logic to skip files that already exist
+# TODO, add case if images don't have category? Or are not in folder
+files_to_upload = {}
+def upload_files(): # dataset_name):
+    global files_to_upload
+    print(f"uploading {sum([len(files_to_upload[f]) for f in files_to_upload.keys()])} files")
+    print(f"filenames {files_to_upload}")
+    dataset_name = config['dataset']['name']
+    for category in files_to_upload.keys():
+        print(f'uploading "{category}" images')
+        dataset_id = ds_ids[dataset_name]['_id']
+        # categories = ds_ids[dataset_name]['categories']
+        # print(f"info for dataset {dataset_name}")
+        print(ds_ids[dataset_name]['categories'])
+        # for name in ds_ids:
+        #     print(name)
+        #     print(ds_ids[name]['categories'])
+
+        if (category not in ds_ids[dataset_name]['categories'].keys()):
+            print(f"category {category} does not exist in dataset {dataset_name}, creating")
+            #TODO
+            c = create_dataset_category(dataset_name, category)
+            ds_ids[dataset_name]['categories'].update(c)
+            # categories = ds_ids[dataset_name]['categories']
+        else:
+            print(f"category {category} already exists in dataset {dataset_name}")
+        categories = ds_ids[dataset_name]['categories']
+        # print(f'categories {categories}')
+        # print(f'category {category}')
+        files = [ ('files', open(file, 'rb') ) for file in files_to_upload[category]]
+        files.append( ("category_name", category))
+        files.append( ("category_id", categories[category]))
+        # print(files)
+        r = requests.post(f"{url}api/datasets/{dataset_id}/files", headers=headers, files=files, verify=False)
+        # print(r)
+        if r.status_code == 200:
+            print(f"{len(files) - 2} {category} files posted to dataset {dataset_id}")
+            global file_upload_count
+            file_upload_count = 0
+        else:
+            print(f"error uploading file(s), HTTP {r.status_code}")
+    files_to_upload = {}
+        # TODO zip if more than 5 files
 
 
+def train_model(dataset_id, action): #action=None, strategy={}):
+    print("training model")
+    t = date.fromtimestamp(time.time()).strftime("%m%d%Y-%H%M%S")
+    # ds_id = ds_ids[name]['id']
+    # dataset_id
+    # name = config['model']['name']
+    if action == "retrain":
+        name = config['model']['name']
+    elif action == "train":
+        # TODO may cause clutter creating a new model every x files. Perhaps we can only keep most recent 5 models or so, remove oldest
+        name = config['model']['name'] + t
+    elif action == "nothing":
+        return
+    body = {
+        "action": "create", # hardcoding to classifier for now. TODO, can add object detection if object coords are included in a json file
+        "dataset_id": dataset_id,
+        # pretrained_model: "", # TODO use in case of retrain?
+        # strategy: strategy,
+        "name": name
+    }
+    r = requests.post(f"{url}api/dltasks", headers=headers, json=body, verify=False)
+    if r.status_code == 200:
+        print(f"training model for dataset {dataset_name}")
+        global file_train_count
+        file_train_count = 0
+    else:
+        print(f"error training model, {r.status_code}")
+        print(f"{r.text}")
+
+
+# On start
+
+# get PAIV token
+token = get_token(config)
+headers = {
+    'X-Auth-Token': token
+}
+
+# get dataset information (categories, files)
+ds_ids = get_datasets()
+dataset_name = config['dataset']['name']
+if dataset_name not in ds_ids.keys():
+    ds_ids[dataset_name] = create_dataset(dataset_name)
+
+
+# TODO, should only get info for dataset in the config file
+# for name in ds_ids:
+    # ds_id = ds_ids[name]['_id']
+    # ds_ids[name]['categories'] = get_dataset_categories(ds_id)
+    # ds_ids[name]['files'] = get_dataset_files(ds_id)
 
 '''
 # examples
-
 ds_ids = {
     "dataset1": {
         id: "4n231ji-12412n",
@@ -163,102 +262,132 @@ files_to_upload = {
     "car": [
         "file1.png",
         "file2.png"
+    ],
+    "glacier": [
+        "file3.png",
+        "file4.png"
     ]
 }
+
+files_to_upload.keys()
 '''
 
+ds_id = ds_ids[dataset_name]['_id']
+ds_ids[dataset_name]['categories'] = get_dataset_categories(ds_id)
+ds_ids[dataset_name]['files'] = get_dataset_files(ds_id)
 
+# ds_ids = get_dataset_info() # should refresh info every x hours
 
-
-
-# TODO, add logic to skip files that already exist
-def upload_files(files_to_upload, dataset_name):
-    print(f"uploading {len(files_to_upload.keys())} files")
-    # TODO, add case if images don't have category? Or are not in folder
-    # dataset_name = config['dataset']['name']
-    for category in files_to_upload.keys():
-        print(f'uploading "{category}" images')
-        dataset_id = ds_ids[dataset_name]['_id']
-        categories = ds_ids[dataset_name]['categories']
-        if (category not in categories.keys()):
-            print(f"category {category} does not exist in dataset {dataset_name}, creating")
-            create_dataset_category(dataset_name, category)
-            categories = ds_ids[dataset_name]['categories']
-        else:
-            print(f"category {category} already exists in dataset {dataset_name}")
-        files = [ ('files', open(file, 'rb') ) for file in files_to_upload[category]]
-        files.append( ("category_name", category))
-        files.append( ("category_id", categories[category]))
-        print(files)
-        # data = {"category_name": category}
-        print(f"posting to {url}api/datasets/{dataset_id}/files")
-        r = requests.post(f"{url}api/datasets/{dataset_id}/files", headers=headers, files=files, verify=False)
-        print(r)
-        if r.status_code == 200:
-            print(f"{len(files) - 2} {category} files posted to dataset {dataset_id}")
-        else:
-            print(f"error uploading file(s), HTTP {r.status_code}")
-    files_to_upload = {}
-        # TODO zip if more than 5 files
-
-def train_model(dataset_id, dataset_name): #action=None, strategy={}):
-    # t = date.fromtimestamp(time.time()).strftime("%m_%d_%Y_%H%M%S")
-    # ds_id = ds_ids[name]['id']
-    # dataset_id
-    # name = config['dataset']['name']
-    body = {
-        action: "create", # hardcoding to classifier for now. TODO, can add object detection if object coords are included in a json file
-        dataset_id: dataset_id,
-        # strategy: strategy,
-        name: dataset_id + t
-    }
-    r = requests.post(f"{url}api/dltasks", headers=headers, json=body, verify=False)
-    if r.status_code == 200:
-        print(f"began training model for {}")
-
-if 'token' not in config['credentials'].keys():
-    get_token(config)
-    config['token'] = token
-    # config['time_token_received'] =
-    headers = {
-        'X-Auth-Token': token
-    }
-else:
-    print(f"token already in config file: {config['credentials']['token']}")
-    token = config['credentials']['token']
-    headers = {
-        'X-Auth-Token': token
-    }
-
-# get_datasets()
-
-
-# get dataset information (categories, files)
-ds_ids = get_datasets()
-
-dataset_name = config['dataset']['name']
-if dataset_name not in ds_ids.keys():
-    ds_ids[dataset_name] = create_dataset(dataset_name)
-
-for name in ds_ids:
-    ds_id = ds_ids[name]['id']
-    ds_ids[name]['categories'] = get_dataset_categories(ds_id)
-    ds_ids[name]['files'] = get_dataset_files(ds_id)
-
-# return ds_ids
 
 
 # Monitor folder, trigger file upload after x number of files gets added
 # global file_count
 # file_count = 0
-files_to_upload = {}
-global file_upload_count
-global file_train_count
+
 file_upload_count = 0
 file_train_count = 0
+
+global upload_timer
+global train_timer
+
+def start_upload_timer():
+    # We're using an timer in case the user adds more files to any of the subfolders after the threshold is met
+    # This will give the user a buffer of x seconds to add more files. Timer is reset every time the
+    upload_thread = list(filter(lambda t: (t.name == 'upload_timer'), threading.enumerate()))
+    if upload_thread: #'upload_timer' in [t.name == 'upload_timer' for t in threading.enumerate()]:
+        print("more files added, resetting upload timer")
+        upload_thread[0].cancel()
+    else:
+        print("files exceed threshold, starting upload timer")
+    upload_timer_wait_time = 5.0
+    print(f"waiting for {upload_timer_wait_time} seconds to upload files")
+    upload_timer = Timer(upload_timer_wait_time, upload_files) #, args=[files_to_upload], name="upload_timer")
+    upload_timer.name = 'upload_timer'
+    upload_timer.start()
+
+
+def reset_upload_timer():
+    # reset upload timer and append additional images IF images match same category
+    print("more files added, resetting upload timer")
+    upload_timer.cancel()
+    # upload_timer.args()
+    # args = args + upload_timer.args()
+    # upload_files(files_to_upload, config['dataset']['name'])
+    upload_timer = Timer(10.0, upload_files, name="upload_timer") #, args=[files_to_upload], name="upload_timer")
+    upload_timer.start()
+
+def wait_for_uploads(train_timer):
+    for i in range(0,3):
+        upload_thread = filter(lambda t: (t.name == 'upload_timer'), threading.enumerate())
+        if upload_thread: #threading.active_count() > 1:
+            print("waiting for uploads to complete")
+            time.sleep(10)
+        else:
+            break
+    train_timer.start()
+
+def start_training_timer():
+    train_thread = list(filter(lambda t: (t.name == 'train_timer'), threading.enumerate()))
+    if train_thread:
+        print("resetting training timer")
+        train_thread[0].cancel()
+    else:
+        print("starting training timer")
+        # print("files exceed threshold, starting upload timer")
+    # t = Timer(10.0, train_model( ds_ids[dataset_name]['_id'], config['model']['action']))
+    train_timer = Timer(10.0, train_model, args=[ds_ids[dataset_name]['_id'], config['model']['action']])
+    train_timer.name = "train_timer"
+    # check if upload thread is running, if so wait for 30 seconds
+    # for i in range(0,3):
+    #     upload_thread = filter(lambda t: (t.name == 'upload_timer'), threading.enumerate())
+    #     if upload_thread: #threading.active_count() > 1:
+    #         print("waiting for uploads to complete")
+    #         time.sleep(10)
+    #     else:
+    #         break
+    # train_timer.start()
+    wait_uploads_thread = Thread(target = wait_for_uploads, args = [train_timer])
+    wait_uploads_thread.start()
+
+
+def reset_training_timer():
+    train_timer.cancel()
+    # check if upload thread is running, if so wait for 25 seconds
+    for i in range(0,5):
+        upload_thread = filter(lambda t: (t.name == 'upload_timer'), threading.enumerate())
+        if upload_thread: #threading.active_count() > 1:
+            print("waiting for uploads to complete")
+            time.sleep(5)
+    train_timer = Timer(5.0, train_model, args=[ds_ids[dataset_name]['_id'], config['model']['action']])
+    train_timer.name = "train_timer"
+    train_timer.start()
+
+'''
+Files are added to subfolder
+
+Determine if the number of files exceed the 'upload' threshold
+
+    If they do, we schedule a thread to upload the files
+
+If additional files are added during the countdown, we'll cancel and reschedule a new upload thread
+
+We'll then see if the files exceed the 'train' threshold
+
+If so, we'll create a training thread
+
+We'll wait until all other upload threads are finished before starting the training thread
+
+'''
+
+total_file_count = 0
 class Event(LoggingEventHandler):
+    # file_upload_count = 0
+    # file_train_count = 0
     def on_created(self, event):
-        print("file added to folder")
+        global total_file_count
+        global file_upload_count
+        global file_train_count
+        print(">>>>>>>>>> new file added <<<<<<<<<<<<")
         print(event.src_path)
         split_path = event.src_path.split('/')
         filename = split_path[-1]
@@ -270,19 +399,59 @@ class Event(LoggingEventHandler):
             print(f"unsupported media type {image_type}")
             return
         files_to_upload[category].append(event.src_path)
+        total_file_count += 1
         file_upload_count += 1
         file_train_count += 1
-        print(f'{file_count} / {threshold}')
+        print(f'upload: {file_upload_count} / {upload_threshold}')
+        print(f'training: {file_train_count} / {training_threshold}')
+        print(f'total_file_count: {total_file_count}')
         if file_upload_count >= upload_threshold:
-            print(f"uploading {file_upload_count} files")
+            print(yellow_text)
+            print(white_text)
+            start_upload_timer()
+            # if 'upload_timer' in [t.name for t in threading.enumerate()]:
+                # reset_upload_timer()
             # if action == "append":
-            upload_files(files_to_upload, config['dataset']['name'])
-            file_upload_count = 0
+            # if ('upload_timer' in vars() or 'upload_timer' in globals()) and (upload_timer.is_alive()):
+            # if threading.active_count() > 1:
+                # reset_upload_timer()
+            # else:
+                # start_upload_timer()
+            # upload_files(files_to_upload, config['dataset']['name'])
+            # print("resetting file_upload_count")
+
         if file_train_count >= training_threshold:
-            print(f"{file_train_count} files")
+            print(green_text)
+            print(f"{file_train_count} files added to folders, training model")
+            print(white_text)
             # upload_files(files_to_upload)
-            train_model( ds_ids[dataset_name]['id'] )
-            file_train_count = 0
+            # TODO, think through, should wait x seconds after training threshold is passed in case user is still adding additional files
+            # We'll have a delay here in case more images are in the process of being copied or uploaded
+            training_delay = 10 # have a delay of 10 seconds before training
+            start_training_timer()
+            # if ('train_timer' in vars() or 'train_timer' in globals()) and (train_timer.is_alive()):
+            # # if threading.active_count() > 1:
+            #     reset_training_timer()
+            # else:
+            #     start_training_timer()
+            #     # getting multiple
+
+            print(f"training model in {training_delay} seconds")
+            # file_train_count = 0
+
+            '''
+            # wait until other upload threads are complete
+            threading.active_count()
+            '''
+            # time.sleep(training_delay)
+            # print("starting timer")
+            # start_timer()
+            # train_model( ds_ids[dataset_name]['_id'], config['model']['action'])
+            # print("resetting file_train_count")
+            print(white_text)
+
+            # time.sleep(3)
+        print("finished handling added file")
 
         # files.append(event.src_path)
 
@@ -290,12 +459,17 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
     # path = config['folders']
     event_handler = Event()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    if config['folders']:
+        for folder in config['folders']:
+            observer.schedule(event_handler, folder, recursive=True)
+    else:
+        path = sys.argv[1] if len(sys.argv) > 1 else '.'
+        observer.schedule(event_handler, path, recursive=True)
     observer.start()
+    print(f"Observing files in {config['folders']}")
     try:
         while True:
             time.sleep(1)
